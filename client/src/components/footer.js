d9e153fef1b73f0e1a161a170367fac12998081f
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2021-04-10 18:39:25 +0100

    initial commit

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { colors, ApolloIcon } from '../styles';
+import React from "react";
+import styled from "@emotion/styled";
+import { colors, ApolloIcon } from "../styles";
 
 /**
  * Footer is useless component to make our app look a little closer to a real website!
  */
 const Footer = ({ children }) => {
+  const currentYear = new Date().getFullYear();
   return (
     <FooterContainer>
-      2021 ©{' '}
+      {currentYear} ©{" "}
       <LogoContainer>
         <ApolloIcon width="100px" height="40px" />
       </LogoContainer>
@@ -20,22 +21,22 @@ export default Footer;
 
 /** Footer styled components */
 const FooterContainer = styled.div({
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
   color: colors.pink.base,
   marginTop: 30,
   height: 200,
   padding: 20,
-  backgroundColor: 'white',
-  borderTop: `solid 1px ${colors.pink.light}`,
+  backgroundColor: "white",
+  borderTop: `solid 1px ${colors.pink.light}`
 });
 
 const LogoContainer = styled.div({
   height: 40,
   marginLeft: 5,
   svg: {
-    height: 40,
-  },
+    height: 40
+  }
 });
